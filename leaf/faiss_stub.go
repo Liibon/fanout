241b1ad3ad47eea9edf023928e6dd91ec697970b
Liibaan Egal <Liibaaneagle@gmail.com>
2026-03-07 09:49:35 -0500

    leaf: add faiss_stub.go for non-CGo builds

--- leaf/faiss_stub.go
+++ leaf/faiss_stub.go
@@ -4,6 +4,8 @@ package main
 
 import "errors"
 
+// NewFaissIndex is unavailable without CGo + FAISS.
+// Build with CGO_ENABLED=1 and FAISS installed (see leaf/Dockerfile).
 func NewFaissIndex(_ *Config) (Index, error) {
 	return nil, errors.New("FAISS requires CGO_ENABLED=1; use Docker to build the leaf")
 }
